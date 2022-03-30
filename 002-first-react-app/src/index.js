4e23bccb8ef8006648cf5eaaebd8cf98572a3d81
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-03-30 22:03:01 +0700

    mostly gone through JSX component rules vs. HTML

--- 002-first-react-app/src/index.js
+++ 002-first-react-app/src/index.js
@@ -2,9 +2,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const buttonText = 'No! Click Me!' // We define a variable here and then reference it later
+const buttonText1 = 123456
+const buttonText2 = ['hi', 'there']
+const buttonText3 = [1,2,3,5,7,9]
+const buttonText4 = { text: 'Click on me again' }
+
+const style = { backgroundColor: 'red', color: 'white' }
+
+const labelText = 'Enter name:'
+
+const getButtonText = () => { // We can also define a function for same purpose
+    return 'Click on Me!'
+}
+
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+
+    return (
+        <div>
+            <label className="label" for="name">{labelText}</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText1}</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText2}</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText3}</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText4.text}</button>
+            <button style={style}>{buttonText4.text}</button>
+        </div>
+    ) // We use the () to return the div as we want it
 }
 
 // Display the react component on the screen
