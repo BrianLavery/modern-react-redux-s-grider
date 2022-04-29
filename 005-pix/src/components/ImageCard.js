96335c269797af8e443fe984bfd96c4d1095c409
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-29 20:46:17 +0700

    completed pix app

--- 005-pix/src/components/ImageCard.js
+++ 005-pix/src/components/ImageCard.js
@@ -4,20 +4,29 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = { spans: 0 }
+
         // This is where we set up a Ref - using this to get image height (we don't use document.querySelector)
         this.imageRef = React.createRef()
     }
 
     componentDidMount() {
-        console.log(this.imageRef)
-        console.log(this.imageRef.current.clientHeight)
+        this.imageRef.current.addEventListener('load', this.setSpans)
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight
+
+        const spans = Math.ceil(height / 10)
+
+        this.setState({ spans })
     }
 
     render() {
         const { description, urls } = this.props.image
 
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
         )
