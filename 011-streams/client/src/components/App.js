f3ab55faf6d832d848c61629b7c31267dd46a738
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-05-09 12:30:17 +0700

    305

--- 011-streams/client/src/components/App.js
+++ 011-streams/client/src/components/App.js
@@ -1,30 +1,28 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-
-const PageOne = () => {
-    return <div>
-        Page One
-        <Link to='/pagetwo'>Navigate to page two</Link>
-    </div>
-}
-
-const PageTwo = () => {
-    return (
-        <div>
-            Page Two
-            <button>Click Me!</button>
-            <Link to='/'>Navigate to page one</Link>
-        </div>
-    )
-}
+// Deployment with BrowserRouter can be challenging using some services
+// Browser Router struggles with unknown path as there is no backend html server (wont' do 404 - will give back html)
+// Hash Router use as server won't look at anything after '#' so only client looks at this
+import { BrowserRouter, HashRouter, MemoryRouter, Route, Link } from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+import Header from './Header'
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div> 
-                    <Route path='/' exact component={PageOne} />
-                    <Route path='/pagetwo' component={PageTwo} />
+                    <Header />
+                    <div className='ui container'>
+                        <Route path='/' exact component={StreamList} />
+                        <Route path='/streams/new' exact component={StreamCreate} />
+                        <Route path='/streams/edit' exact component={StreamEdit} />
+                        <Route path='/streams/delete' exact component={StreamDelete} />
+                        <Route path='/streams/show' exact component={StreamShow} />
+                    </div>
                 </div>
             </BrowserRouter>
         </div>
@@ -32,3 +30,34 @@ const App = () => {
 }
 
 export default App
+
+
+
+// const PageOne = () => {
+//     return <div>
+//         Page One
+//         <Link to='/pagetwo'>Navigate to page two</Link>
+//     </div>
+// }
+
+// const PageTwo = () => {
+//     return (
+//         <div>
+//             Page Two
+//             <button>Click Me!</button>
+//             <Link to='/'>Navigate to page one</Link>
+//         </div>
+//     )
+// }
+// const App = () => {
+//     return (
+//         <div>
+//             <BrowserRouter>
+//                 <div> 
+//                     <Route path='/' exact component={PageOne} />
+//                     <Route path='/pagetwo' component={PageTwo} />
+//                 </div>
+//             </BrowserRouter>
+//         </div>
+//     )
+// }
