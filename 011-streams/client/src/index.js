4ab735ee1da12ad1861e14905d4ee73e968c140e
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-05-12 15:10:30 +0700

    beginning on video streaming

--- 011-streams/client/src/index.js
+++ 011-streams/client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import reduxThunk from 'redux-thunk'
+const NodeMediaServer = require('node-media-server');
 
 import App from './components/App'
 import { reducers, authReducer } from './reducers'
